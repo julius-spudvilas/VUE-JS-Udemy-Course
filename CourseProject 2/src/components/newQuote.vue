076acbec93bb7d012ab9b2767fe4f9baf4f47839
Julius Spudvilas <julius.spudvilas@gmail.com>
2018-08-15 10:17:41 +0200

    progress

--- CourseProject 2/src/components/newQuote.vue
+++ CourseProject 2/src/components/newQuote.vue
@@ -6,7 +6,7 @@
                 <textarea name="" id="" cols="30" rows="3" class="form-control" v-model="quote"></textarea>
             </div>
             <div class="col-sm-8 col-sm-offset-2 col-xs-12 col-md-6 col-md-offset-3 form-group">
-                <button class="btn btn-primary" @click.prevent=createNew>Add Quote</button>
+                <button class="btn btn-primary" @click.prevent="createNew">Add Quote</button>
             </div>
         </form>
     </div>
